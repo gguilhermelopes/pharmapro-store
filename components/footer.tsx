61301f81dee06bb025de616784d544d025598ef3
gguilhermelopes <guilhermelopes275@gmail.com>
2023-07-17 16:17:38 -0300

    feat: create navbar

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,8 @@ const Footer = () => {
     <footer className="bg-white border-t">
       <div className="mx-auto py-10">
         <p className="text-center text-sm text-black">
-          <strong>&copy; pharmaPro</strong> <p>Todos os direitos reservados</p>
+          <strong>&copy; pharmaPro</strong> <br />
+          Todos os direitos reservados
         </p>
       </div>
     </footer>
